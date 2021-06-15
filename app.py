974ebf5d5a5f53fb0bb705ab475c9f4192889706
oqtavios <3875187+oqtavios@users.noreply.github.com>
2021-06-15 15:51:23 +0300

    Added api methods

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from random import randint
+from random import randint, choice
 
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, abort, jsonify
 
 from imdb_parser import *
 from scheduled_cheker import CheckerScheduler
 
 app = Flask(__name__)
+checkerScheduler = CheckerScheduler()
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html",
                            lucky_list=lucky_list)
+
 
 @app.route('/card', methods=['POST'])
 def card():
@@ -47,6 +50,64 @@ def card():
                                movie_info=actor_info[8])
 
 
+@app.route('/api/v1/search', methods=['GET'])
+def search():
+    name = None
+    if 'feelingLucky' in request.args and request.args.get('feelingLucky', type=int) == 1:
+        name = choice(lucky_list)
+    elif 'name' in request.args:
+        name = request.args.get('name', type=str)
+    else:
+        abort(400)
+
+    actors = request_js([name[0], name.replace(' ', '_')])
+    if len(actors) > 0:  # todo: actor found on imdb
+        return jsonify({'results': actors, 'feelingLucky': False}), 200
+    else:
+        return jsonify(), 404
+
+
+@app.route('/api/v1/actor', methods=['GET'])
+def actor():
+    if 'id' in request.args and request.args.get('id', type=str).startswith('nm'):
+        # TODO: actor details
+        return jsonify({'actor': {}}), 200
+    else:
+        abort(400)
+
+
+@app.route('/api/v1/subscription', methods=['PUT'])
+def addSubscription():
+    if 'id' in request.args and 'firebaseToken' in request.args:
+        actorId = request.args.get('id', type=str)
+        firebaseToken = request.args.get('firebaseToken', type=str)
+
+        if check_actor_existence(actorId) is not None:
+            try:
+                checkerScheduler.addSubscription(actorId=actorId, firebaseToken=firebaseToken)
+            except ValueError:
+                abort(400)
+            return jsonify(), 201
+        else:
+            abort(404)
+    else:
+        abort(400)
+
+
+@app.route('/api/v1/subscription', methods=['DELETE'])
+def removeSubscription():
+    if 'id' in request.args and 'firebaseToken' in request.args:
+        actorId = request.args.get('id', type=str)
+        firebaseToken = request.args.get('firebaseToken', type=str)
+
+        try:
+            checkerScheduler.removeSubscription(actorId=actorId, firebaseToken=firebaseToken)
+            return jsonify(), 200
+        except ValueError:
+            abort(404)
+    else:
+        abort(400)
+
+
 if __name__ == '__main__':
-    checkerScheduler = CheckerScheduler()
     app.run()
