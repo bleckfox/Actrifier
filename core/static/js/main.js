d01ffa4d7ea42f6bbf465507987ae29e9dea4377
bleckfox <79881790355@ya.ru>
2021-06-17 00:14:39 +0300

    Fix - if search field is empty we will not start search

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     function get_json_info () {
         var field = $('input');
         var value = field.val();
@@ -10,19 +11,30 @@ $(document).ready(function () {
         }
     }
     function search () {
+
         var btn = $(".search_btn");
         btn.click( function (event) {
+            console.log('search');
+            var form = $('form');
+            var value = $('input').val();
+            if (value !== ''){
+                form.attr('action', '/card');
+                form.attr('method', 'post');
+            }
+            else {
+                alert('Введите имя актера или нажмите "I\'m Feeling Lucky"')
+            }
+        });
+    }
+    function lucky () {
+
+        var btn = $(".lucky_btn");
+        btn.click( function (event) {
+            console.log('lucky');
             var form = $('form');
             //$.post('/card');
             form.attr('action', '/card');
             form.attr('method', 'post');
-
-        });
-    }
-    function lucky () {
-        var btn = $(".lucky_btn");
-        btn.click( function (event) {
-            $.post('/card');
         })
     }
     search();
